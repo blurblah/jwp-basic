7a475d0416f7b8fbfe1f6d718e951ef539e9680c
Sunchan Lee <blurblah@blurblah.net>
2017-09-04 20:01:10 +0900

    회원정보 수정기능 구현

---
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -0,0 +1,26 @@
+package next.web;
+
+import core.db.DataBase;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sunchanlee on 2017. 9. 4..
+ */
+@WebServlet("/user/updateForm")
+public class UpdateUserFormServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        req.setAttribute("user", DataBase.findUserById(userId));
+        RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
+        rd.forward(req, resp);
+    }
+}
